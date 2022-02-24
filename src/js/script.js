f80bc3ea5b0b776ee0d98093ad5a4490d222b8bb
Alex Smit <AlexSmit17264@gmail.com>
2022-02-24 20:34:15 +0300

    add four page, carousel

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed:1200,
+        adaptiveHeight:true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/next.svg"></button>',
+        /* responsive: */
+      });
+  });
