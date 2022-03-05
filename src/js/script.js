2e1abfaf85527cd54f4e0f34396dceb089d2e12a
Alex Smit <AlexSmit17264@gmail.com>
2022-03-05 12:10:32 +0300

    add support files

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,25 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../img/next.svg"></button>',
         /* responsive: */
       });
+
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+        $(this)
+        .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content__active').eq($(this).index()).addClass('catalog__content__active');
+      });
+
+
+      function toggleSlide(item){
+          $(item).each(function(i){
+              $(this).on('click',function(e){
+                e.preventDefault();
+                $('.catalog--item__content').eq(i).toggleClass('catalog--item__content__active');
+                $('.catalog--item__list').eq(i).toggleClass('catalog--item__list__active')
+              })              
+          })
+      }
+      toggleSlide('.catalog--item__link')
+      toggleSlide('.catalog--item__back')
+
   });
